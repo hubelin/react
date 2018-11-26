dba81fa8e5f6a1a536f2e9d5f0e727c9d33be359
Hubert <huberthflin@gmail.com>
2018-11-25 20:09:01 -0800

    completed Tabs exercise

--- react-appacademy/frontend/Widget.jsx
+++ react-appacademy/frontend/Widget.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Clock from './Clock';
+import Tabs from './Tabs';
 
 const Root = () => {
   return (
     <div>
       <Clock />
+      <Tabs panes={panes} />
     </div>
   );
 };
@@ -14,3 +16,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('main');
   ReactDOM.render(<Root />, root);
 });
+
+const panes = [
+  { title: 'one', content: 'I am the first' },
+  { title: 'two', content: 'Second pane here' },
+  { title: 'three', content: 'Third pane here' }
+];
